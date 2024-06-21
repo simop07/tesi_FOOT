f33518bff3013892fd015fe7d38b0810cf5e7c60
Simone Pasquini <simo.pp07@gmail.com>
2024-06-21 12:58:19 +0200

    Update values

--- progetto/constants.h
+++ progetto/constants.h
@@ -3,25 +3,27 @@
 // https://wiki.jlab.org/ciswiki/images/4/49/EJ-200.pdf
 // https://pdg.lbl.gov/2018/AtomicNuclearProperties/HTML/polyvinyltoluene.html
 
+// values of constants are found here
+// https://pdg.lbl.gov/2021/reviews/rpp2020-rev-passage-particles-matter.pdf
+// https://physics.nist.gov/cgi-bin/cuu/Value?mu
+
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
 namespace constants {
 inline constexpr double Z{0.54141};
 inline constexpr double I{64.7};                      // in eV
-inline constexpr double rho{1023.};                   // in kg*m^{-3}
+inline constexpr double rho{1032.};                   // in kg*m^{-3}
 inline constexpr double N_a{6.02214076e23};           // in mol^{-1}
-inline constexpr double m_e_kg{9.1093837e-31};        // in kg
-inline constexpr double m_e_eV{0.51099895000e6};      // in ev
-inline constexpr double epsilon_0{8.8541878128e-12};  // in C^2*N^{-1}*m^{-2}
-inline constexpr double q_e{1.60217663e-19};          // in C
+inline constexpr double m_e_eV{0.51099895069e6};      // in ev
+inline constexpr double epsilon_0{8.8541878188e-12};  // in C^2*N^{-1}*m^{-2}
+inline constexpr double q_e{1.602176634e-19};         // in C
 inline constexpr double c{299792458.};                // in m/s
-inline constexpr double u{0.9315};                    // in GeV
-inline constexpr double U{1.660538921e-27};           // in kg
+inline constexpr double u{0.93149410372};             // in GeV
+inline constexpr double M_u{1.00000000105e-3};        // in kg/mol
 inline constexpr double k_energy_u_beam{0.200};       // in GeV
 inline constexpr double d_SC_TGT{30.};                // in cm
 inline constexpr double dx{6e-3};                     // in m
-inline constexpr double number_mass_beam{12.};
 }  // namespace constants
 
 #endif
