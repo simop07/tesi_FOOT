97feb41c5d8d043cfe266039a15e3883bd3f8a01
Simone Pasquini <simo.pp07@gmail.com>
2024-06-21 12:58:28 +0200

    Update name

--- progetto/constants.h
+++ progetto/constants.h
@@ -4,8 +4,7 @@
 // https://pdg.lbl.gov/2018/AtomicNuclearProperties/HTML/polyvinyltoluene.html
 
 // values of constants are found here
-// https://pdg.lbl.gov/2021/reviews/rpp2020-rev-passage-particles-matter.pdf
-// https://physics.nist.gov/cgi-bin/cuu/Value?mu
+// https://physics.nist.gov/cuu/Constants/index.html
 
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
@@ -15,7 +14,7 @@ inline constexpr double Z{0.54141};
 inline constexpr double I{64.7};                      // in eV
 inline constexpr double rho{1032.};                   // in kg*m^{-3}
 inline constexpr double N_a{6.02214076e23};           // in mol^{-1}
-inline constexpr double m_e_eV{0.51099895069e6};      // in ev
+inline constexpr double m_e{0.51099895069e6};         // in ev
 inline constexpr double epsilon_0{8.8541878188e-12};  // in C^2*N^{-1}*m^{-2}
 inline constexpr double q_e{1.602176634e-19};         // in C
 inline constexpr double c{299792458.};                // in m/s
